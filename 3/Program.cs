8c3a8af48a590d65cd1ac6511cd7ba5d549b278a
Nikolay Alekhin <kolyan.alekhin@mail.ru>
2022-12-03 15:19:04 +0300

    pers4

---
+++ 3/Program.cs
@@ -0,0 +1,19 @@
+﻿
+/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+
+
+
+
+
+Console.WriteLine("Введите  число");
+string namberStr = Console.ReadLine();
+int namber = Convert.ToInt32(namberStr);
+for (int i = 2; i <= namber; i=i+2)
+{
+   Console.Write($"{i},");
+}
